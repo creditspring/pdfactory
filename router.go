c242850132c9e5c4ebd1c6ff360025c860c5dddc
Dawid Matusiewicz <dawid.matusiewicz@CS-M-00221.local>
2026-02-23 13:56:02 +0100

    Exporting DPI as environment variable

--- router.go
+++ router.go
@@ -3,16 +3,33 @@ package main
 import (
 	"encoding/base64"
 	"fmt"
-	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"os"
+	"strconv"
+
+	"github.com/gin-gonic/gin"
 )
 
 func Router() *gin.Engine {
+	var dpiUint uint
 	router := gin.Default()
 
 	user := os.Getenv("USER")
 	password := os.Getenv("PASSWORD")
+	dpiString := os.Getenv("PDF_DPI")
+
+	if dpiString != "" {
+		dpi, err := strconv.Atoi(dpiString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		dpiUint = uint(dpi)
+	} else {
+		dpiUint = 1200
+	}
+
+	fmt.Printf("Info: PDF dpi set to: %d\n", dpiUint)
 
 	var authorized *gin.RouterGroup
 	if user != "" && password != "" {
@@ -36,7 +53,7 @@ func Router() *gin.Engine {
 	authorized.POST("/pdf", func(c *gin.Context) {
 		html := c.PostForm("html")
 
-		pdf, err := GeneratePDF(html)
+		pdf, err := GeneratePDF(html, dpiUint)
 		if err != nil {
 			fmt.Println(err)
 			c.Writer.WriteHeader(http.StatusUnprocessableEntity)
